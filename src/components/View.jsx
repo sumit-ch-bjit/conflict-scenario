f31825a5b07a88ca30c047da84284074a153bc0d
Shakhawat <shakhawat.hossain@bjitacademy.com>
2023-08-11 12:09:57 +0600

    view component

---
+++ src/components/View.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const View = () => {
+    return (
+        <div>
+            <h1>This is View</h1>
+        </div>
+    );
+};
+
+export default View;
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -2,6 +2,8 @@ const Post = () => {
     return (
         <div>
             this is a new post
+            Changing Here
+            
         </div>
     )
 }
